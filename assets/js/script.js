7de2d4e384ffdd6ea535a1fee1f345ff76c9690d
Chubbs91 <chaskin1991@outlook.com>
2021-12-16 14:53:48 -0600

    Questions and answers added

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,54 @@ var quizQuestions = [{
     ChoiceD: "getElementsById()",
     correctAnswer: "c" },
 {
-
-}
+    question: "What HTML attribute references an external JavaScript file?",
+    ChoiceA: "class",
+    ChoiceB: "href",
+    ChoiceC: "index",
+    ChoiceD: "src",
+    correctAnswer: "d" },
+{
+    question: "The first index of an Array is?",
+    ChoiceA: "5",
+    ChoiceB: "2",
+    ChoiceC: "1",
+    ChoiceD: "0",
+    correctAnswer: "d" },
+{
+    question: "What does DOM stand for?",
+    ChoiceA: "Document Object Model",
+    ChoiceB: "Desktop Oriented Mode",
+    ChoiceC: "Data Object Model",
+    ChoiceD: "Display Object Management",
+    correctAnswer: "a" },
+{
+    question: "Commonly used data types DO NOT include:",
+    ChoiceA: "Booleans",
+    ChoiceB: "Strings",
+    ChoiceC: "Alerts",
+    ChoiceD: "Numbers",
+    correctAnswer: "c" },
+{
+    question: "The condition of an if/else statement is enclosed in?",
+    ChoiceA: "Curly brackets",
+    ChoiceB: "Quotes",
+    ChoiceC: "Square brackets",
+    ChoiceD: "Parentheses",
+    correctAnswer: "d" },
+{
+    question: "Where in an HTML file should a Script tag be placed?",
+    ChoiceA: "Inside and on the bottom of the Body Element",
+    ChoiceB: "Outside the HTML element",
+    ChoiceC: "Inside of the Header Element",
+    ChoiceD: "The placement of the script tag does not matter",
+    correctAnswer: "a" },
+{
+    question: "Which of the following is NOT a way to declare a variable?",
+    ChoiceA: "var",
+    ChoiceB: "set",
+    ChoiceC: "let",
+    ChoiceD: "const",
+    correctAnswer: "b" },
 ];
 
 //Additional Variables
