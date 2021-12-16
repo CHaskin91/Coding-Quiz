06093a5d17bcc283b06bbabededcd1a64368f443
Chubbs91 <chaskin1991@outlook.com>
2021-12-16 17:08:18 -0600

    added rest of HTML variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,16 @@ var resultsEl = document.getElementById("result");
 var finalScoreEl = document.getElementById("finalScore");
 var gameoverDiv = document.getElementById("gameover");
 var questionsEl = document.getElementById("questions")
+var quizTimer = document.getElementById("timer");
 var startQuizButton = document.getElementById("startbtn");
 var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score")
 var buttonA = document.getElementById("a");
 var buttonA = document.getElementById("b");
 var buttonA = document.getElementById("c");
@@ -71,6 +79,7 @@ var quizQuestions = [{
 ];
 
 //Additional Variables
+var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
 var correct;
 
