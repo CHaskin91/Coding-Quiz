e6ec788bd6805005cb07b31d1c82b0c5ecf4e9fa
Chubbs91 <chaskin1991@outlook.com>
2021-12-16 13:53:17 -0600

    added first question/generate and start quiz to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,48 @@
+// HTML Variables
+var gameoverDiv = document.getElementById("gameover")
 var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var buttonA = document.getElementById("a");
+var buttonA = document.getElementById("b");
+var buttonA = document.getElementById("c");
+var buttonA = document.getElementById("d");
 
+// Quiz Questions
+var quizQuestions = [{
+    question: "Which method would you use to find an ID element?",
+    ChoiceA: "getElementsById()",
+    ChoiceB: "getElementbyId()",
+    ChoiceC: "getElementByID()",
+    ChoiceD: "getElementsById()",
+    correctAnswer: "b" },
+{
+    
+}
+]
+
+// Function to generate the Quiz
+function generateQuizQuestion() {
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex) {
+        return showScore();
+    }
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+// Start Quiz Function
+function startQuiz() {
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuizQuestion();
+
+    // Timer will go here
+
+}
 
 // This button will start the quiz
 startQuizButton.addEventListener("click", startQuiz);
