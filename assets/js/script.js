b24401c735577a49d79e33095acaf3a6df8d85b8
Chubbs91 <chaskin1991@outlook.com>
2021-12-18 14:33:48 -0600

    clear score function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,6 +168,23 @@ function generateHighscores() {
 }
 
 // Show High Score Function
+function showHighscore() {
+    startQuizDiv.style.display = "none";
+    gameoverDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    highscoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+// Clear Score Function
+function clearScore() {
+    window.localStorage.clear();
+    highscoreDisplayName.textContent = "";
+    highscoreDisplayScore.textContent = "";
+}
+
 
 //Check Answer Function
 function checkAnswer(answer) {
